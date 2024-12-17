557458f5f68c19f3145e1ba3963f06b04d9a50c0
Matheus Andrade <matheus@tidal.com>
2024-12-17 18:51:10 +0100

    unit tests sorting

--- country-server/src/test/java/com/ztl/countryservice/service/CountryServiceTest.java
+++ country-server/src/test/java/com/ztl/countryservice/service/CountryServiceTest.java
@@ -1,17 +1,32 @@
 package com.ztl.countryservice.service;
 
+import com.ztl.countryservice.controller.SortDirection;
 import com.ztl.countryservice.domain.Country;
 import com.ztl.countryservice.domain.Currency;
 import com.ztl.countryservice.domain.Name;
+import com.ztl.countryservice.service.externalservice.RestCountriesService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class CountryServiceTest {
+    @Mock
+    RestCountriesService restCountriesService;
+
+    @InjectMocks
+    CountryService service;
+
 
     @Test
     void when_groupingCountries_correctlyGrouped() {
@@ -24,6 +39,36 @@ class CountryServiceTest {
         assertThat(countriesByCurrency).isEqualTo(Map.of("NOK", Set.of("Norway"),
             "USD", Set.of("United States", "Caribbean Netherlands", "Panama"),
             "PAB", Set.of("Panama")));
+    }
+
+    @Test
+    void when_sortingEuropeanCountries_asc_correctlySorted() {
+        List<Country> givenCountries = new ArrayList<>(List.of(norway(), usa(), panama(), caribbeanNetherlands()));
+        when(restCountriesService.getEuropeanCountries()).thenReturn(givenCountries);
+
+        List<Country> sortedCountries = service.getEuropeanCountries(SortDirection.ASC);
+
+        assertThat(sortedCountries).isEqualTo(List.of(caribbeanNetherlands(), norway(), panama(), usa()));
+    }
+
+    @Test
+    void when_sortingEuropeanCountries_desc_correctlySorted() {
+        List<Country> givenCountries = new ArrayList<>(List.of(norway(), usa(), panama(), caribbeanNetherlands()));
+        when(restCountriesService.getEuropeanCountries()).thenReturn(givenCountries);
+
+        List<Country> sortedCountries = service.getEuropeanCountries(SortDirection.DESC);
+
+        assertThat(sortedCountries).isEqualTo(List.of(usa(), panama(), norway(), caribbeanNetherlands()));
+    }
+
+    @Test
+    void when_sortingEuropeanCountries_noSort_sameOrderAsReturned() {
+        List<Country> givenCountries = new ArrayList<>(List.of(norway(), usa(), panama(), caribbeanNetherlands()));
+        when(restCountriesService.getEuropeanCountries()).thenReturn(givenCountries);
+
+        List<Country> sortedCountries = service.getEuropeanCountries(SortDirection.NO_SORT);
+
+        assertThat(sortedCountries).isEqualTo(List.of(norway(), usa(), panama(), caribbeanNetherlands()));
     }
 
 
